86b17101c92667d68873c91d4e1d7ad4a2f2dfc6
Lucas <lucas.crenais@gmail.com>
2021-08-27 10:40:23 +0200

    controlller update & cleaning useless codelines

--- Controller/ConfigurationController.php
+++ Controller/ConfigurationController.php
@@ -35,31 +35,27 @@ class ConfigurationController extends BaseAdminController
         }
 
         $url = '/admin/module/ProductStatus';
-
         $form = $this->createForm(StatusContentForm::getName());
-
         $errorMsg = null;
+
+        if (null !== $response = $this->checkAuth(
+                [AdminResources::MODULE],
+                [ProductStatus::DOMAIN_NAME],
+                AccessManager::UPDATE)) {
+            return $response;
+        }
 
         try {
             $validForm = $this->validateForm($form);
-
             $productStatus = new \ProductStatus\Model\ProductStatus();
 
-            if ($validForm->get('status-name')->getData() &&
-                $validForm->get('status-code')->getData() &&
-                $validForm->get('color')->getData() &&
-                $validForm->get('info-text')->getData()
-            )
-
-            {
-                $productStatus
-                    ->setLocale($this->getSession()->getAdminEditionLang()->getLocale())
-                    ->setTitle($validForm->get('status-name')->getData())
-                    ->setCode($validForm->get('status-code')->getData())
-                    ->setColor($validForm->get('color')->getData())
-                    ->setDescription($validForm->get('info-text')->getData())
-                    ->save();
-            }
+            $productStatus
+                ->setLocale($this->getSession()->getAdminEditionLang()->getLocale())
+                ->setTitle($validForm->get('status-name')->getData())
+                ->setCode($validForm->get('status-code')->getData())
+                ->setColor($validForm->get('color')->getData())
+                ->setDescription($validForm->get('info-text')->getData())
+                ->save();
 
         } catch (\Exception $e) {
             $this->setupFormErrorContext(
